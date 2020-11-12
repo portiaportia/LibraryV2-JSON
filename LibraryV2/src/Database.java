acb70ea6e5d8bddd2212d7be011486d3ccf6791d
portiaportia <portiaplante@gmail.com>
2020-11-12 11:23:08 -0500

    adding example

---
+++ LibraryV2/src/Database.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Database {
+	
+	public static ArrayList<User> getUsers(){
+		return new ArrayList<User>();
+	}
+	
+	public static ArrayList<Item> getItems(){
+		return new ArrayList<Item>();
+	}
+}
